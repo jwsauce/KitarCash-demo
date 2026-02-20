f3d261b6ecc1974950480e86da128a6e36b58357
Jin Wei <89194495+jwsauce@users.noreply.github.com>
2026-02-20 18:47:37 +0800

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBZcsmaKr8TOQ7Tqwa65z4gh6ZCpdlYhlk",
+  authDomain: "kitarcash.firebaseapp.com",
+  projectId: "kitarcash",
+  storageBucket: "kitarcash.firebasestorage.app",
+  messagingSenderId: "929687561236",
+  appId: "1:929687561236:web:d85694e588600e866cf301"
 };
 
 // Initialize Firebase
